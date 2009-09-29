e142340cd70b9c0964cedb8c9941f0ef238f5e6f
Darwin <darwin.git@marianna.se>
2009-09-29 16:16:16 +0200

    Changed from uuid to a counter starting on time.now for the ids of the emails sent with mock_smtp_facde

--- emailer.gemspec
+++ emailer.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{emailer}
-  s.version = "0.1.15"
+  s.version = "0.1.16"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Erik Hansson", "Bjorn Blomqvist"]
-  s.date = %q{2009-09-21}
+  s.date = %q{2009-09-29}
   s.description = %q{}
   s.email = %q{erik@bits2life.com}
   s.extra_rdoc_files = [
--- lib/emailer/mock_smtp_facade.rb
+++ lib/emailer/mock_smtp_facade.rb
@@ -1,32 +1,34 @@
 require 'uuid'
 
 module Emailer
-  
+
   class MockNetSmtp
     def start(*args); end
     def sendmail(*args); end
     def finish; end
     def started?; end
   end
-  
+
   class MockSmtpFacade < SmtpFacade
-  
+
     attr_reader :sent
-    
+
     def initialize(settings = {})
       @sent = {}
+      @count = Time.now.to_i
       super
     end
-    
+
     # Don't open connections...
     def get_net_smtp_instance
       MockNetSmtp.new
     end
-    
+
     # And save, don't send, mail...
     def send_mail(options)
       raise ConnectionNotOpenError unless @open
-      @sent[UUID.new.generate] = options
+      @sent[@count.to_s] = options
+      @count += 1
       true
     rescue ConnectionNotOpenError => e
       raise e
@@ -35,24 +37,23 @@ module Emailer
       @offending_mail = options
       false
     end
-    
+
     def last_email_sent
       @sent[@sent.keys.last.to_s]
     end
-    
+
     def last_email_sent_key
       @sent.keys.last
     end
-    
+
     def last_email_sent_url
       get_url_for last_email_sent_key
     end
-    
+
     def get_url_for uuidString
-       TestingMiddleware.testing_path+uuidString
+      TestingMiddleware.testing_path+uuidString
     end
   end
-  
-end
 
+end
 
